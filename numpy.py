0e45fcfd604362d7c975310cd751f034925cb8e0
raghavgla <106462940+raghavgla@users.noreply.github.com>
2022-05-29 13:23:28 +0530

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,3 @@
+Do Numpy And Scipy Support Python 3.x?
+NumPy and SciPy support the Python 2.x series, (versions 2.6 and 2.7), as well as Python 3.2 and newer. 
+The first release of NumPy to support Python 3 was NumPy 1.5.0. Python 3 support in SciPy starts with version 0.9.0.
