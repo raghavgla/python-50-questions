f0bc640127ed15ccb6e70e01218e04f4d7994e6f
raghavgla <106462940+raghavgla@users.noreply.github.com>
2022-05-29 13:06:12 +0530

    Create question 9.py

---
+++ dictionary/question 9.py
@@ -0,0 +1,9 @@
+#Create a dictionary by extracting the keys from a given dictionary
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# Keys to extract
+keys = ["name", "salary"]
