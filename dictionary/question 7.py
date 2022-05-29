1a32fd645ab9672a1675cf53415f8c06a3cade99
raghavgla <106462940+raghavgla@users.noreply.github.com>
2022-05-29 13:05:13 +0530

    Create question 7.py

---
+++ dictionary/question 7.py
@@ -0,0 +1,10 @@
+# Using a loop and update() method of a dictionary
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+# empty dictionary
+res_dict = dict()
+
+for i in range(len(keys)):
+    res_dict.update({keys[i]: values[i]})
+print(res_dict)
