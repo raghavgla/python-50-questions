3ab2cb720a411d65923f5862f1b02be584b5cad8
raghavgla <106462940+raghavgla@users.noreply.github.com>
2022-05-29 13:08:52 +0530

    Create question 3.py

---
+++ random/question 3.py
@@ -0,0 +1,3 @@
+ Difference between random.uniform( ) and random.random( )?
+ For generation of floating-point random number uniform( ) and random( ) is used. 
+ The difference is that random( ) generates a random number between 0 to 1, while with uniform( ), a range can be passed to the method.
